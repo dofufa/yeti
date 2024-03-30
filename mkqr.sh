453f2093575cc99d2a1593cb8cc5d4ee697e660c
Dofufa Labs <121826629+dofufa@users.noreply.github.com>
2024-03-29 18:56:34 -0700

    Update mkqr.sh

--- mkqr.sh
+++ mkqr.sh
@@ -3,8 +3,7 @@
 ## mkqr.sh
 ## JOB      : grabs qrcode
 ## GIT      : https://github.com/dofufa/yeti
-## LIC      : MIT
-
+## LIC      : https://opensource.org/license/mit
 
 if [[ -z $1 ]]; then
  echo 'z-exit'
@@ -21,9 +20,14 @@ fi
 id=$1
 
 qrfile="qr@${id}.png"
-
 watchurl="https://www.youtube.com/watch?v=$id"
+
+## using solution found here:
+## https://stackoverflow.com/a/34407620
+## https://gist.github.com/sergeiwaigant/f0cf816e143114b6a7d81a901b5d3ce6
+xurl=$(printf %s $watchurl|jq -sRr @uri)
+
 base="https://chart.googleapis.com/chart?"
-params="cht=qr&chs=75x75&choe=utf-8&chl=${watchurl}"
+params="cht=qr&chs=75x75&choe=utf-8&chl=${xurl}"
 qrurl=$base$params
-wget -O "qr/$qrfile" "$qrurl"
+#wget -O "qr/$qrfile" "$qrurl"
